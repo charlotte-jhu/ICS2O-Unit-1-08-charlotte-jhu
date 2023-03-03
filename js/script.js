74f56a00ff19bcbcf079c362d97680a86eefbe30
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-03-03 18:16:37 +0000

    added inital code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  alert("You clicked the button!");
+}
