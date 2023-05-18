243350965febfce4a8aabb12bf4f8b1e4f5b612c
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-05-18 16:18:18 +0000

    changed html

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@
 // Created on: March 2023
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-Unit-1-08-charlotte-jhu/",
+  })
+}
+
 function myButtonClicked() {
+  // this function shows an alert
   alert("You clicked the button!");
 }
